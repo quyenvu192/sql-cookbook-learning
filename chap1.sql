d462a4060f14e29e6d0017eec4defc937a79f586
Quyen Vu <vutranhoangquyen@gmail.com>
2023-08-01 21:44:30 +0100

    nguuuuuuuuuuuu

--- chap1.sql
+++ chap1.sql
@@ -1,3 +1,8 @@
-aasdfa
-thay doi lan 1
-thay doi lan 2
+-- 1.1 Retrieving All Rows and Columns from a Table
+
+SELECT * from emp;
+
+
+--- 1.2 Retrieving a Subset of Rows from a Table
+
+select * from emp e where deptno = 10;
